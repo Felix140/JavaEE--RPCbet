d78301ad4cc07a33a9846ba3a140917a567dc39e
BracciL <lorenzobracci@outlook.com>
2023-07-08 12:04:45 +0200

    servlet registrazione utente
    aggiunta servlet registrazione utente + modifica campo user utente to username utente

---
+++ RPCbet_project/src/main/java/Servlet/Servlet_RegistazioneUtente.java
@@ -0,0 +1,35 @@
+package Servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class Servlet_RegistazioneUtente
+ */
+public class Servlet_RegistazioneUtente extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String nome = request.getParameter("nome");
+		String cognome = request.getParameter("cognome");
+		String username_utente = request.getParameter("username_utente");
+		String codice_fiscale = request.getParameter("codice_fiscale");
+		String numero_documento = request.getParameter("numero_documento");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		String saldo = request.getParameter("saldo");
+		String data_nascita = request.getParameter("data_nascita");
+
+		
+		// TODO Auto-generated method stub
+	}
+
+}
